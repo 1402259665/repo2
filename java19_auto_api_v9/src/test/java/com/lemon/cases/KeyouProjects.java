bde8a4db3959bb771d249021ccb7dd52eb5a98c1
wangcan <1402259665@qq.com>
2020-09-08 21:33:49 +0800

    添加v9到本地仓库

---
+++ java19_auto_api_v9/src/test/java/com/lemon/cases/KeyouProjects.java
@@ -0,0 +1,74 @@
+package com.lemon.cases;
+
+import com.lemon.constants.Constants;
+import com.lemon.pojo.CaseInfo;
+import com.lemon.utils.AuthenticationUtils;
+import com.lemon.utils.ExcelUtils;
+import com.lemon.utils.HttpUtils;
+import io.qameta.allure.Description;
+import org.apache.http.HttpResponse;
+import org.apache.log4j.Logger;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.util.Map;
+
+/**
+ * @author luojie
+ * @date 2020/6/11 - 21:34
+ * 柠檬班创新教育极致服务
+ *
+ * keyou项目项目列表用例
+ */
+public class KeyouProjects extends BaseCase {
+
+    private static Logger logger = Logger.getLogger(KeyouProjects.class);
+
+    @Test(dataProvider = "datas",description = "keyou项目列表description属性")
+    @Description("description 注解")
+
+    public void test(CaseInfo caseInfo) {
+        //  1、参数化替换
+        paramsReplace(caseInfo);
+        //	2、数据库前置查询结果(数据断言必须在接口执行前后都查询)
+        //  2.1、获取带token的请求头
+        Map<String, String> headers = AuthenticationUtils.getKeyouTokenHeader();
+//        Set<String> keySet = headers.keySet();
+//        for(String key:keySet){
+//            System.out.println("headers=="+headers.get(key));
+//        }
+        //	3、调用接口
+        HttpResponse response = HttpUtils.call(caseInfo.getMethod(),caseInfo.getContentType()
+                ,caseInfo.getUrl(),caseInfo.getParams(), headers);
+        String body = HttpUtils.printResponse(response);
+        //  3.1、从响应体里面获取token
+//        AuthenticationUtils.json2Vars(body,"$.data.token_info.token","${token}");
+        //  3.2、从响应体里面获取member_id
+//        AuthenticationUtils.json2Vars(body,"$.data.id","${member_id}");
+        //	4、断言响应结果
+        //{"$.code":0,"$.msg":"OK","$.data.mobile_phone":"15670890431"}
+        boolean assertResponseFlag = assertResponse(body, caseInfo.getExpectResult());
+
+        //	5、添加接口响应回写内容
+        addWriteBackData(caseInfo.getId(),Constants.RESPONSE_WRTIE_BACK_CELLNUM,body);
+        //	6、数据库后置查询结果
+        //	7、据库断言
+//        	8、添加断言回写内容
+        String assertResult = assertResponseFlag ? "passed" : "failed";
+        addWriteBackData(caseInfo.getId(),Constants.ASSERT_WRTIE_BACK_CELLNUM,assertResult);
+        //	9、添加日志
+        //	10、报表断言
+        Assert.assertEquals(assertResult,"passed");
+    }
+
+
+
+
+    @DataProvider
+    public Object[] datas() {
+        Object[] datas = ExcelUtils.getDatas(sheetIndex, 1, CaseInfo.class);
+        return datas;
+    }
+
+}
